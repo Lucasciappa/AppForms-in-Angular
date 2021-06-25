6464dead7bfae610df3d18fd8a87ad16f34fad3e
lucasciappa <lucasciappa97@gmail.com>
2021-06-24 23:42:54 -0300

    first commit templates end

---
+++ src/app/template/basicos/basicos.component.ts
@@ -0,0 +1,45 @@
+import { Component, Input, OnInit, ViewChild } from '@angular/core';
+import { NgForm } from '@angular/forms';
+
+@Component({
+  selector: 'app-basicos',
+  templateUrl: './basicos.component.html',
+  styles: [
+  ]
+})
+export class BasicosComponent implements OnInit {
+
+  @ViewChild("miFormulario") miFormulario!: NgForm
+
+  initForm = {
+    producto: "RTX 4080ti",
+    precio: 10,
+    existencias: 10
+  }
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+  nombreValido(): boolean{
+    return  this.miFormulario?.controls.producto?.invalid && this.miFormulario?.controls.producto?.touched
+  }
+
+  precioValido(): boolean{
+    return this.miFormulario?.controls.precio?.value < 0 && this.miFormulario?.controls.producto?.touched
+      
+  }
+
+  guardar(){
+    console.log(this.miFormulario);
+
+    console.log("Posteo Correcto");
+    this.miFormulario.resetForm({
+      producto: "Algo",
+      precio: 0,
+      existencias: 0
+    })
+  }
+
+}
